69735731dd0ffd44d8920b71d527a7b8e62d791b
laure leter <laure.leter@gmail.com>
2021-06-15 19:32:54 +0200

    install react-icons + front setup ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FaBirthdayCake  as IdIcon} from 'react-icons/fa';
+import { MdGavel as BulletinIcon} from 'react-icons/md';
+import { GrUserExpert as ExpertIcon} from 'react-icons/gr';
+import { BsPeopleFill as SocialIcon} from 'react-icons/bs';
+import { GiInjustice as JapIcon} from 'react-icons/gi';
+import { GiPrisoner as SpipIcon} from 'react-icons/gi';
+
+
+
+
+import events from "./events";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
 
 function App() {
+  let idIconStyles = { background: "#58f" };
+  let bulletinIconStyles = { background: "#ff5733"};
+  let expertIconStyles = { background: "#f9c74f"};
+  let socialIconStyles = { background: "#18b05a"};
+  let japIconStyles = { background: " #cd69d1 "};
+  let spipIconStyles = { background: "#db626c"};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Exemple de timeline</h1>
+      <VerticalTimeline>
+        {events.map((element) => {
+          let isIdIcon = element.icon === "id";
+          let isExpertIcon = element.icon === "expert";
+          let isBulletinIcon = element.icon === "bulletin";
+          let isSocialIcon = element.icon === "social";
+          let isJapIcon = element.icon === "jap";
+          let isSpipIcon = element.icon === "spip";
+
+
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== "";
+
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isIdIcon ? idIconStyles : isExpertIcon ? expertIconStyles: isBulletinIcon ? bulletinIconStyles: isSocialIcon ? socialIconStyles: isJapIcon ? japIconStyles:  isSpipIcon ? spipIconStyles: expertIconStyles }
+              icon={isIdIcon ? <IdIcon /> : isExpertIcon ?  <ExpertIcon />: isBulletinIcon ? <BulletinIcon/>: isSocialIcon ? <SocialIcon />: isJapIcon ? <JapIcon />: isSpipIcon ? <SpipIcon />: <ExpertIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {element.title}
+              </h3>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.location}
+              </h5>
+              <p id="description">{element.description}</p>
+              {showButton && (
+                <a
+                  className={`button ${
+                    isIdIcon ? "idButton" : isExpertIcon ? "expertButton": isBulletinIcon ? "bulletinbutton" : isSocialIcon ? "socialbutton" : isJapIcon ? "japbutton" : isSpipIcon ? "spipbutton" : "expertButton"
+                  }`}
+                  href="/"
+                >
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
     </div>
   );
 }
---
+++ src/events.js
@@ -0,0 +1,64 @@
+let events = [
+  {
+    id: 1,
+    title: "John Doe",
+    location: "Né le 14 décembre 1990, à Paris",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    buttonText: "Voir la fiche d'identité",
+    date: "1990",
+    icon: "id",
+  },
+  {
+    id: 2,
+    title: "B1",
+    location: "Nantes",
+    description:
+      "condamnation 1",
+    buttonText: "Voir le bulletin",
+    date: "10 juin 2021",
+    icon: "bulletin",
+  },
+  {
+    id: 3,
+    title: "Expertise pyschologique",
+    location: "Expert: xx xx",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    buttonText: "Voir le rapport",
+    date: "September 2020",
+    icon: "expert",
+  },
+  {
+    id: 4,
+    title: "Enquête sociale",
+    location: "Enquêteur: xx xx",
+    description:
+     "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    buttonText: "Voir l'enquête",
+    date: "September 2020",
+    icon: "social",
+  },
+  {
+    id: 5,
+    title: "Suivi JAP",
+    location: "juge: xxxx",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+    buttonText: "Voir le dossier",
+    date: "6 juillet 2020",
+    icon: "jap",
+  },
+  {
+    id: 6,
+    title: "Avis SPIP",
+    location: "auteur: xx xx",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+    date: "2 juin 2020",
+    icon: "spip",
+  },
+];
+
+export default events;
+
