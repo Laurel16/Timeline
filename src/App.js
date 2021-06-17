91ba0cb64de29caec62a1fc06a6e1986c5e4f9b0
laure leter <laure.leter@gmail.com>
2021-06-17 23:12:31 +0200

    little changes in seed + conditionnal rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { FaBirthdayCake  as IdIcon} from 'react-icons/fa';
 import { MdGavel as BulletinIcon} from 'react-icons/md';
+import { VscFileSubmodule as ProcedureIcon} from 'react-icons/vsc';
 import { GrUserExpert as ExpertIcon} from 'react-icons/gr';
 import { BsPeopleFill as SocialIcon} from 'react-icons/bs';
 import { GiInjustice as JapIcon} from 'react-icons/gi';
 import { GiPrisoner as SpipIcon} from 'react-icons/gi';
+import { BiCheck} from 'react-icons/bi';
+import { FaRegAddressCard} from 'react-icons/fa';
+import { ImProfile} from 'react-icons/im';
+
+
 
 
 
@@ -22,9 +29,10 @@ import "react-vertical-timeline-component/style.min.css";
 function App() {
   let idIconStyles = { background: "#58f" };
   let bulletinIconStyles = { background: "#ff5733"};
-  let expertIconStyles = { background: "#f9c74f"};
+  let procedureIconStyles = { background: "#f9c74f"};
+  let expertIconStyles = { background: "#18b05a"};
   let socialIconStyles = { background: "#18b05a"};
-  let japIconStyles = { background: " #cd69d1 "};
+  let japIconStyles = { background: " #db626c "};
   let spipIconStyles = { background: "#db626c"};
 
   return (
@@ -35,6 +43,7 @@ function App() {
           let isIdIcon = element.icon === "id";
           let isExpertIcon = element.icon === "expert";
           let isBulletinIcon = element.icon === "bulletin";
+          let isProcedureIcon = element.icon === "procedures";
           let isSocialIcon = element.icon === "social";
           let isJapIcon = element.icon === "jap";
           let isSpipIcon = element.icon === "spip";
@@ -45,15 +54,38 @@ function App() {
             element.buttonText !== null &&
             element.buttonText !== "";
 
+          let showListe =
+            element.liste !== undefined &&
+            element.liste !== null &&
+            element.liste !== "";
+
+          let showTag2 =
+            element.tag2 !== undefined &&
+            element.tag2 !== null &&
+            element.tag2 !== "";
+
+          let showAddress =
+            element.address !== undefined &&
+            element.address !== null &&
+            element.address !== "";
+
+           let showNumber =
+            element.number !== undefined &&
+            element.number !== null &&
+            element.number !== "";
+
           return (
             <VerticalTimelineElement
               key={element.key}
               date={element.date}
               dateClassName="date"
-              iconStyle={isIdIcon ? idIconStyles : isExpertIcon ? expertIconStyles: isBulletinIcon ? bulletinIconStyles: isSocialIcon ? socialIconStyles: isJapIcon ? japIconStyles:  isSpipIcon ? spipIconStyles: expertIconStyles }
-              icon={isIdIcon ? <IdIcon /> : isExpertIcon ?  <ExpertIcon />: isBulletinIcon ? <BulletinIcon/>: isSocialIcon ? <SocialIcon />: isJapIcon ? <JapIcon />: isSpipIcon ? <SpipIcon />: <ExpertIcon />}
+              iconStyle={isIdIcon ? idIconStyles : isExpertIcon ? expertIconStyles: isBulletinIcon ? bulletinIconStyles: isSocialIcon ? socialIconStyles: isJapIcon ? japIconStyles:  isSpipIcon ? spipIconStyles: isProcedureIcon ? procedureIconStyles: expertIconStyles }
+              icon={isIdIcon ? <IdIcon /> : isExpertIcon ?  <ExpertIcon />: isBulletinIcon ? <BulletinIcon/>: isSocialIcon ? <SocialIcon />: isJapIcon ? <JapIcon />: isSpipIcon ? <SpipIcon />: isProcedureIcon ? <ProcedureIcon />: <ExpertIcon />}
             >
             <Button variant="outline-dark" className="tag"> {element.tag}</Button>
+            {showTag2 && (
+              <Button variant="outline-dark" className="tag2"> {element.tag2}</Button>
+              )}
               <h3 className="vertical-timeline-element-title">
                 {element.title}
               </h3>
@@ -61,13 +93,29 @@ function App() {
                 {element.location}
               </h5>
               <h5 className="vertical-timeline-element-subtitle">
-                {element.condamnation}
+                {element.tribunal}
               </h5>
+              {showListe && (
+                <ListGroup>
+                  <ListGroup.Item className="list" id="liste"><BiCheck /> {element.liste}</ListGroup.Item>
+                   <ListGroup.Item className="list" id="liste"><BiCheck /> {element.liste}</ListGroup.Item>
+                </ListGroup>
+
+                   )}
+              { showAddress && (
+                 <p className= "identity" id="address"><FaRegAddressCard/>  {element.address}</p>
+                )
+              }
+              { showNumber && (
+                 <p className= "identity" id="number"><ImProfile/>  {element.number}</p>
+                )
+              }
+
               <p id="description">{element.description}</p>
               {showButton && (
                 <a
                   className={`button ${
-                    isIdIcon ? "idButton" : isExpertIcon ? "expertButton": isBulletinIcon ? "bulletinbutton" : isSocialIcon ? "socialbutton" : isSpipIcon ? "spipbutton" : isJapIcon ? "japbutton" : "expertButton"
+                    isIdIcon ? "idButton" : isExpertIcon ? "expertButton": isBulletinIcon ? "bulletinbutton" : isSocialIcon ? "socialbutton" : isSpipIcon ? "spipbutton" : isJapIcon ? "japbutton" : isProcedureIcon ? "proceduresbutton" : "expertButton"
                   }`}
                   href="/"
                 >
--- src/events.js
+++ src/events.js
@@ -3,9 +3,9 @@ let events = [
     id: 1,
     title: "John Doe",
     tag: "Identité",
-    location: "Né le 14 décembre 1990, à Paris",
-    description:
-      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    location: "Né le 14 décembre 1990, à Paris - 31 ans",
+    address: "6, rue des Prairies, Nantes",
+    number: "121354456677XSP",
     buttonText: "Voir la fiche d'identité",
     date: "1990",
     icon: "id",
@@ -13,18 +13,32 @@ let events = [
   {
     id: 2,
     title: "Bulletin n°1",
-    tag: "casier judiciaire",
-    description:
-      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    tag: "Casier judiciaire",
+    liste:
+      "There are many variations of passages of Lorem Ipsum available.",
     buttonText: "Voir le bulletin",
     date: "10 juin 2021",
     icon: "bulletin",
   },
-  {
+
+   {
     id: 3,
+    title: "Autres procedures en cours",
+    tag: "Procédures",
+    tribunal: "Tribunal civil/correctionnel",
+    liste:
+      "But the majority have suffered alteration in some form.",
+    buttonText: "Voir les procedures",
+    date: "juin 2021",
+    icon: "procedures",
+  },
+
+  {
+    id: 4,
     title: "Expertise pyschologique",
     tag: "Enquête de personalité",
-    location: "Expert: ",
+    tag2: "Psychologie",
+    location: "Expert: Jane Doe",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
     buttonText: "Voir le rapport",
@@ -32,10 +46,11 @@ let events = [
     icon: "expert",
   },
   {
-    id: 4,
+    id: 5,
     title: "Enquête sociale",
     tag: "Enquête de personalité",
-    location: "Enquêteur: ",
+    tag2:"Social",
+    location: "Enquêteur: John Doe",
     description:
      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
     buttonText: "Voir l'enquête",
@@ -43,10 +58,11 @@ let events = [
     icon: "social",
   },
   {
-    id: 5,
+    id: 6,
     title: "Suivi JAP",
     tag: "Application des peines",
-    location: "juge: ",
+    tag2: "JAF",
+    location: "Juge: Jane Doe",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
     buttonText: "Voir le dossier",
@@ -54,10 +70,11 @@ let events = [
     icon: "jap",
   },
   {
-    id: 6,
+    id: 7,
     title: "Avis SPIP",
     tag: "Application des peines",
-    location: "auteur: ",
+    tag2: "SPIP",
+    location: "Auteur: John Doe",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
     buttonText: "Voir l'avis",
